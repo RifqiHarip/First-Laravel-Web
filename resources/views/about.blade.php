9306fc17cb65e7eed2f7c2792919945fe9f48b93
Rifqi Hari Putranto <rifqiharip@gmail.com>
2024-03-27 14:22:41 +0700

    NavBar with Blade Template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 
 @section('container')        
-    <h1>Welcome Page</h1>
+    <h1>About Page</h1>
     <a href="{{ URL::to('/') }}">Welcome</a>
     <a href="{{ URL::to('/about') }}">About</a>
     <a href="{{ URL::to('/home') }}">Home Page</a>
-    <h2>This is my welcome page</h2>
+    <h2>This is my about page</h2>
 @endsection   
